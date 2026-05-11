9ba31d97a9763395388cd1d638e8c5b043382460
Dimitris Iracleous <iracleous@gmail.com>
2026-05-11 19:01:52 +0300

    Testing Jwt

--- TestProjectLoans/WebTests/LoanApiTests.cs
+++ TestProjectLoans/WebTests/LoanApiTests.cs
@@ -1,8 +1,12 @@
 ﻿ 
 
 namespace TestProjectLoans.WebTests;
- 
+
+using FluentAssertions;
+using SubscriberApi.Dtos;
 using System.Net;
+using System.Net.Http.Headers;
+using System.Net.Http.Json;
 
 public class LoanApiTests
     : IClassFixture<CustomWebApplicationFactory>
@@ -47,5 +51,87 @@ public class LoanApiTests
         // Assert
         response.EnsureSuccessStatusCode();
     }
+
+
+    //Authorization test
+    [Fact]
+    public async Task ShouldNotReadMessageWithoutAuthorization()
+    {
+
+        var  response = await 
+            _client.GetAsync("/api/auth/secureMessage", TestContext.Current.CancellationToken);
+
+        //Assert
+        response.StatusCode
+           .Should()
+           .Be(HttpStatusCode.Unauthorized);
+
+    }
+
+    [Fact]
+    public async Task Login_ReturnsToken()
+    {
+        var loginRequest = new
+        {
+            username = "admin",
+            password = "1234"
+        };
+
+        var response =
+            await _client.PostAsJsonAsync(
+                "/api/auth/login",
+                loginRequest, TestContext.Current.CancellationToken);
+
+        response.StatusCode
+            .Should()
+            .Be(HttpStatusCode.OK);
+
+        var result =
+            await response.Content
+                .ReadFromJsonAsync<LoginResponse>(cancellationToken: TestContext.Current.CancellationToken)
+                ?? throw new InvalidOperationException("Failed to deserialize login response.");
+
+        result.Token.Should().NotBeNull();
+
+
+
+    }
+
+    [Fact]
+    public async Task GetLoans_WithValidToken_Returns200()
+    {
+        // Login first
+        var loginRequest = new
+        {
+            username = "admin",
+            password = "1234"
+        };
+
+        var loginResponse =
+            await _client.PostAsJsonAsync(
+                "/api/auth/login",
+                loginRequest, TestContext.Current.CancellationToken);
+
+        var tokenResult =
+            await loginResponse.Content
+                .ReadFromJsonAsync<LoginResponse>(TestContext.Current.CancellationToken) ?? throw new InvalidOperationException("Failed to deserialize login response.");
+
+        // Attach JWT
+        _client.DefaultRequestHeaders.Authorization =
+            new AuthenticationHeaderValue(
+                "Bearer",
+                tokenResult.Token);
+
+        // Call secured endpoint
+        var response =
+            await _client.GetAsync("/api/auth/secureMessage", TestContext.Current.CancellationToken);
+
+        response.StatusCode
+            .Should()
+            .Be(HttpStatusCode.OK);
+    }
+
+
+
 
 }
